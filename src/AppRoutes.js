e6c21fb5ed61a1f8262e1bfacc2d61d435230612
RAKESHRAJ MAHAKUD <rakeshraj.mahakud03@gmail.com>
2022-08-13 14:37:35 +0530

    updated

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import {Index} from './Pages/Index'
+import {Home} from './Pages/Home'
+import {NotFound} from './Pages/NotFound'
+import { Routes,Route } from 'react-router-dom';
 export const AppRoutes = () => {
     return (
-
+       
+        <Routes>
+            <Route path="/" element={<Index/>} />
+            <Route path="/home" element={<Home/>} />
+            <Route path="*" element={<NotFound/>} />
+        </Routes>
+       
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
-
+        <nav>
+            <ul>
+                <li><NavLink className="index-link" to="/">Index</NavLink></li>
+                <li><NavLink className="home-link" to="/home">Index</NavLink></li>
+            </ul>
+        </nav>
     )
 }
